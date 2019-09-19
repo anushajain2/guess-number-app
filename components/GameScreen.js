e261c86f55bd0d458f4b058a8ea8cf9847785e37
anushajain2 <fhl.anusha@gmail.com>
2019-09-19 23:49:59 +0530

    added basic second screen

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -1,14 +1,65 @@
-import React from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, Button, StyleSheet, Alert } from "react-native";
 
 import Colors from "../constants/colors";
+import NumberContainer from "./NumberContainer";
+import Card from "./Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
 
 const GameScreen = props => {
-  return <View style={styles.container}></View>;
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  const []
+
+  const nextGuessHandler = direction => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("This is incorrect", "Try Again Buddy");
+      return;
+    }
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, "lower")} />
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, "upper")}
+        />
+      </Card>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  container: {}
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
+  }
 });
 
 export default GameScreen;
